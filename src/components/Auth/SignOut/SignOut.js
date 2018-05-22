c70066aa8b57ee31e1a1ca9dff9dfb33e969fbcb
shanellewebster <webstershanelle@gmail.com>
2018-05-21 21:02:31 -0500

    Auth Done

---
+++ src/components/Auth/SignOut/SignOut.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { doSignOut } from "../../../firebase/auth";
+
+const SignOutButton = () => (
+  <button type="button" onClick={doSignOut}>
+    Sign Out
+  </button>
+);
+
+export default SignOutButton;
