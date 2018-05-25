eaaf2b3bcadb040396806960be39d2464cc4ef6b
shanellewebster <webstershanelle@gmail.com>
2018-05-24 20:44:57 -0500

    conflict

--- src/ducks/classRoomReducer.js
+++ src/ducks/classRoomReducer.js
@@ -13,14 +13,8 @@ export default function classRoomReducer(state = initialState, action) {
     case `${SUBMIT_CLASSROOM}_FULFILLED`:
       return Object.assign({}, state, { classRooms: action.payload.data });
 
-<<<<<<< HEAD
-    case `${GET_CLASSROOM}_FULFILLED`:
-      console.log(action);
-      return Object.assign({}, state, { classRoom: action.payload.data });
-=======
     case `${GET_OWNER_CLASSES}_FULFILLED`:
       return Object.assign({}, state, { classRooms: action.payload.data });
->>>>>>> master
     default:
       return state;
   }
@@ -38,16 +32,9 @@ export function submitClassRoom(ownerid, className, password, subject) {
   };
 }
 
-<<<<<<< HEAD
-export function getClassroom() {
-  return {
-    type: GET_CLASSROOM,
-    payload: axios.get(`/api/classroom/2`)
-=======
 export function getOwnerClasses(ownerid) {
   return {
     type: GET_OWNER_CLASSES,
     payload: axios.get(`/api/classes/${ownerid}`)
->>>>>>> master
   };
 }
