8386d94ded199523d42e8fe1c9421d101389b849
shanellewebster <webstershanelle@gmail.com>
2018-05-30 16:22:53 -0500

    material ui

--- src/components/Classroom/ClassView/Teacher View/TeacherView.js
+++ src/components/Classroom/ClassView/Teacher View/TeacherView.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import Flashcards from "../../../Flashcards/Flashcards";
-import { Tabs, Tab } from "material-ui/Tabs";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+// import SwipeableViews from "react-swipeable-views";
+// import Typography from "@material-ui/core/Typography";
 import "./TeacherView.css";
 import { getQuiz } from "../../../../ducks/quizReducer";
 import StudentAvg from "../../../Graphs/TeacherGraphs/TeacherAverageStudentBar";
@@ -10,11 +15,8 @@ import ClassAvg from "../../../Graphs/TeacherGraphs/TeacherAverageClassBar";
 import AnswerAvg from "../../../Graphs/TeacherGraphs/TeacherAverageAnswerBar";
 
 const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400
+  root: {
+    flexGrow: 1
   }
 };
 
@@ -22,7 +24,7 @@ class TeacherView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "a"
+      value: 0
     };
   }
   componentDidMount() {
@@ -35,14 +37,14 @@ class TeacherView extends Component {
     }
   }
 
-  handleTab = value => {
-    this.setState({
-      value: value
-    });
+  handleTab = (event, value) => {
+    this.setState({ value: +value });
   };
-  launchQuiz() {}
 
   render() {
+    const { classes } = this.props;
+    // const { value } = this.state;
+
     if (this.props.loading) {
       return <div>is loading...</div>;
     }
@@ -63,41 +65,45 @@ class TeacherView extends Component {
     //     date={e.date} </div>
     //   )
     // })
-
+    console.log(typeof this.state.value);
     return (
       <div>
-        <Tabs value={this.state.value} onChange={this.handleTab}>
-          <Tab label="Resources" value="a">
-            <div>
-              <Flashcards />
-            </div>
-          </Tab>
-          <Tab label="Quizzes" value="b">
-            <div>
-              <h2 style={styles.headline}>Quizzes</h2>
-              <Link to={`/createquiz/${this.props.match.params.id}`}>
-                CREATE QUIZ
-              </Link>
-              {classQuiz}
-            </div>
-          </Tab>
-          <Tab label="Graphs" value="c">
-            <div>
-              <h2 style={styles.headline}>Analytics</h2>
-              <div className="teachergraphs">
-                <div className="graphs">
-                  <StudentAvg />
-                </div>
-                <div className="graphs">
-                  <ClassAvg />
-                </div>
-                <div className="graphs">
-                  <AnswerAvg />
-                </div>
+        <Tabs value={this.state.value} onChange={this.handleTab} centered>
+          <Tab label="Resources" value="0" />
+          <Tab label="Quizzes" value="1" />
+          <Tab label="Graphs" value="2" />
+        </Tabs>
+
+        {this.state.value === 0 && (
+          <div>
+            <Flashcards />
+          </div>
+        )}
+        {this.state.value === 1 && (
+          <div>
+            <h2 style={styles.headline}>Quizzes</h2>
+            <Link to={`/createquiz/${this.props.match.params.id}`}>
+              CREATE QUIZ
+            </Link>
+            {classQuiz}
+          </div>
+        )}
+        {this.state.value === 2 && (
+          <div>
+            <h2 style={styles.headline}>Analytics</h2>
+            <div className="teachergraphs">
+              <div className="graphs">
+                <StudentAvg />
+              </div>
+              <div className="graphs">
+                <ClassAvg />
+              </div>
+              <div className="graphs">
+                <AnswerAvg />
               </div>
             </div>
-          </Tab>
-        </Tabs>
+          </div>
+        )}
       </div>
     );
   }
@@ -109,4 +115,11 @@ function mapStateToProps(state) {
     quiz: state.quizReducer.quiz
   };
 }
-export default withRouter(connect(mapStateToProps, { getQuiz })(TeacherView));
+
+TeacherView.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+export default withStyles(styles, { withTheme: true })(
+  withRouter(connect(mapStateToProps, { getQuiz })(TeacherView))
+);
